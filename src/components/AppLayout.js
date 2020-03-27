701d3fd2659a34211428178d2f92ad56a7fc3651
evilboss <jr.evilboss@gmail.com>
2020-03-28 01:31:41 +0800

    reverted to simple

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -2,27 +2,23 @@ import React from "react";
 import {Layout, Menu} from 'antd';
 import PropTypes from 'prop-types';
 
-const {Header, Content,Sider} = Layout;
+const {Header, Content} = Layout;
 
 const AppLayout = props => {
     const {children} = props;
     return (
         <Layout className="layout">
-
-            <Sider className="site-layout-background" width={200}>
-                <Header style={{backgroundColor: '#1976d2'}}
+            <Header style={{backgroundColor: '#1976d2'}}
+            >
+                <div className="logo" style={{color: 'white'}}>Whispir-radio</div>
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
                 >
-                    <div className="logo" style={{color: 'white'}}>Whispir-radio</div>
-                    <Menu
-                        theme="dark"
-                        mode="horizontal"
-                        defaultSelectedKeys={['2']}
-                    >
-                    </Menu>
-                </Header>
-            </Sider>
-
-            <Content style={{padding: '0 50px', height: '100vh'}}>
+                </Menu>
+            </Header>
+            <Content style={{padding: '0 50px', height: '90vh'}}>
                 <div className="site-layout-content">
                     {children}
                 </div>
