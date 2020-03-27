acf175256411a5f1460391fe7457588651ec2b24
evilboss <jr.evilboss@gmail.com>
2020-03-27 23:24:37 +0800

    add final layout

--- src/Product.js
+++ src/Product.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Card, Typography} from "antd";
+import {Card, Typography, Row, Col, Divider} from "antd";
 import RadioGroup from './radioGroup';
 
 const {Text} = Typography;
@@ -7,14 +7,34 @@ const {Text} = Typography;
 const Product = (props) => {
     const {options} = props;
     let [optionValue, setOptionValue] = useState('$120');
+    const style = { background: '#0092ff', padding: '8px 0' };
+
     return (
-        <Card title="Product tiers" bordered={false} style={{width: 300}}>
-            <RadioGroup options={options} optionValue={optionValue} setOptionValue={setOptionValue}/>
-            <p>Total Price</p>
-            <Text code>
-                {optionValue} per month
-            </Text>
-        </Card>);
+        <div>
+            <Divider orientation="left">
+
+            </Divider>
+            <Row gutter={16}>
+                <Col className="gutter-row" span={6}>
+                </Col>
+                <Col className="gutter-row" span={12}>
+                    <Card title="Product tiers" bordered={false} style={{width: '100%'}}>
+                        <RadioGroup options={options} optionValue={optionValue} setOptionValue={setOptionValue}/>
+                        <p>Total Price</p>
+                        <Text code>
+                            {optionValue} per month
+                        </Text>
+                    </Card>
+                </Col>
+
+                <Col className="gutter-row" span={6}>
+                </Col>
+            </Row>
+
+
+
+        </div>
+    );
 };
 
 
